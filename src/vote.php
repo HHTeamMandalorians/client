720b747cab1e8717cc009d1c8ae4824e0019f7e2
Filip Gliszczyński <filipgliszczynski@gmail.com>
2022-10-12 17:13:56 +0200

    possible question handle idea

---
+++ src/vote.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>'AppName' - vote</title>
+</head>
+<body>
+    <form action="vote.php" method="post">
+        <label for="answer">
+            <?php
+                //connect to questions data and echo it as text
+            ?>
+        </label>
+        <?php
+            //connect to possible answers data and echo it as radio
+        ?>
+    </form>
+</body>
+</html>

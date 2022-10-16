e52ffdbb7066ab4836ebb4b4fcb2ab64745b07d2
PolskiGlizda <filipgliszczynski@gmail.com>
2022-10-16 19:02:30 +0200

    modified:   src/ts/index.ts(added part of form handler)

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,16 @@
 import * as $ from 'jquery';
+function generateOptions(){
+    let candidates = /api/v1/candidates;
+    let htmlValue = '';
+    for(let i = 0; i < candidates.length(); i++){
+        htmlValue = htmlValue + '<option value="' + i.toString() + '">' + candidates[i] + '</option>';
+    }
+    return htmlValue;
+}
 $(function(){
-
+    $('#answer').html(generateOptions());
+    $(document).on('submit', 'form.form' function(){
+        let opt = $('#answer').attr('selected', true).html();
+        // TODO send to database
+    })
 })
